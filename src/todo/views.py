5f39e656316f5df5dbef3d6786f1b7e42ba351c3
Thibault <houdon.thibault@gmail.com>
2021-03-29 11:42:31 +0200

    Clean files

--- src/todo/views.py
+++ src/todo/views.py
@@ -1,10 +1,7 @@
 from django.http import JsonResponse, HttpResponseRedirect
-from django.shortcuts import render
 from datetime import datetime
-from django.utils.decorators import method_decorator
 from django.urls import reverse_lazy, reverse
-from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, CreateView, DeleteView
 
 from todo.models import ToDo
 
